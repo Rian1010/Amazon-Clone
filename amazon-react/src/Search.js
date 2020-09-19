5f172fc483215c78df815d04dbf6c72ca62df461
Rian1010 <rtegally1098@gmail.com>
2020-09-19 02:26:55 +0200

    Managed to deploy the app correctly

--- amazon-react/src/Search.js
+++ amazon-react/src/Search.js
@@ -6,12 +6,12 @@ import SearchProduct from "./SearchProduct";
 function Search() {
     let searchQ = window.location.href.split("=");
     let searchPercentage = decodeURI(searchQ[1]);
-    let query = searchPercentage.replace("+", " ");
-    console.log("TRIMMED ", searchPercentage)
+    let removeDots = searchPercentage.replace("...", "")
+    let query = removeDots.replace("+", " ");
 
     return (
         <div className="search">
-            {query && productList.filter(item => item.title.toLowerCase().includes(query.toLowerCase())).length > 0? productList.map(item => {
+            {query && productList.filter(item => item.title.toLowerCase().includes(query.toLowerCase())).length > 0 ? productList.map(item => {
                 if (item.title.toLowerCase().includes(query.toLowerCase())) {
                     return <SearchProduct 
                         id={item.prodID}
@@ -21,7 +21,7 @@ function Search() {
                         rating={item.rating} 
                     />
                 } 
-            }) : <h1 className="search__notAvailable">{searchQ[1] ? "Sorry, "  + query + "is not available! (As this is a fake website, only the products available on the home page can be searched)" : "You haven't entered a search query"}</h1>}
+            }) : <h1 className="search__notAvailable">{searchQ[1] ? "Sorry, "  + query + " is not available! (As this is a fake website, only the products available on the home page can be searched)" : "You haven't entered a search query"}</h1>}
         </div>
     )
 }
--- amazon-react/src/axios.js
+++ amazon-react/src/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:5001/clone-568b3/us-central1/api", // THE API (cloud function) URL
+    baseURL: "https://us-central1-clone-568b3.cloudfunctions.net/api", // THE API (cloud function) URL
+    // Got that link from the functions tab on the firebase link that gets generated through firebase deploy --only functions
 });
 
 export default instance;
+
+// http://localhost:5001/clone-568b3/us-central1/api
